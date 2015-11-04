bb6dd68dc4bb2668abd06dd391c34902b05916c6
iam4x <iam4x@me.com>
2015-11-04 14:52:21 +0100

    chore: publish v1.0.2 && tests

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+## v1.0.2 (4 nov 2015)
+
+* Copy static methods from original component to decorated one (fix #1)
+* Add simple tests
+* Add `CHANGELOG.md`
+
+## v1.0.1 (4 nov 2015)
+
+* Add documentation about providing `flux` through `context` with `AltContainer`
+
+## v1.0.0 (3 nov 2015)
+
+* First release
---
+++ src/__tests__.js
@@ -0,0 +1,30 @@
+import test from 'tape';
+import React, { Component } from 'react';
+
+import connect from './src/main';
+
+function noop() {};
+
+test('should decorate component', function(t) {
+  class Dummy extends Component {}
+  const decorated = connect(noop)(Dummy);
+
+  t.equal(decorated.name, 'ConnectToStoresWrapper');
+  t.equal(typeof decorated.contextTypes, 'object');
+
+  t.end();
+});
+
+test('should copy static methods on decorated component', function(t) {
+  class Dummy extends Component {
+    static fetchData() {}
+    static fetchData_ = noop
+    static fetchData__ = () => noop()
+  }
+  const decorated = connect(noop)(Dummy);
+
+  t.equal(typeof decorated.fetchData, 'function');
+  t.equal(typeof decorated.fetchData_, 'function');
+  t.equal(typeof decorated.fetchData__, 'function');
+  t.end();
+});
