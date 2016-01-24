4e1953e5965f95f04895270158998119566f7caa
iam4x <iam4x@me.com>
2016-01-24 17:39:54 +0100

    feat: support specific store listen

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ function getDisplayName(WrappedComponent) {
   return WrappedComponent.displayName || WrappedComponent.name || 'Component'
 }
 
-export default function connectToStores(reducer) {
+export default function connectToStores(...connectArgs) {
   return function(DecoratedComponent) {
     class ConnectToStoresWrapper extends PureComponent {
 
@@ -19,20 +19,82 @@ export default function connectToStores(reducer) {
       static displayName = `Connect(${getDisplayName(DecoratedComponent)})`
       static decoratedComponent = DecoratedComponent
 
-      state = { customProps: reducer(this.takeSnapshot()) };
+      constructor(props, context) {
+        super(props, context);
+
+        // if we have a reducer fn as first arg we
+        // will call this fn on the full alt state
+        if (typeof connectArgs[0] === 'function') {
+          this.state = {
+            connectType: 'reducerFn',
+            customProps: connectArgs[0](this.takeSnapshot())
+          };
+        } else if (typeof connectArgs[0] === 'string') {
+          // if it's string it will be stores names,
+          // we want to provide the stores state into props
+          const { flux } = this.context;
+          this.state = {
+            connectType: 'storesState',
+            customProps: connectArgs.reduce((states, storeName) =>
+              ({
+                ...states,
+                [storeName + 'Store']: flux.getStore(storeName).getState()
+              }), {})
+          }
+        }
+      }
 
       componentDidMount() {
+        const { connectType } = this.state;
         const { flux } = this.context;
-        flux.FinalStore.listen(this.handleStoresChange);
 
-        // a store update may have been updated between state
-        // initialization and listening for changes from store
-        return this.handleStoresChange();
+        switch (connectType) {
+          case 'reducerFn':
+            flux.FinalStore.listen(this.handleStoresChange);
+            // a store update may have been updated between state
+            // initialization and listening for changes from store
+            this.handleStoresChange();
+            break;
+
+          case 'storesState':
+            // bind every store to a listener
+            connectArgs.forEach((storeName) => {
+              const listener = this.handleStoreChange(storeName);
+              this.listeners = this.listeners ?
+                [ ...this.listeners, { [storeName]: listener } ] :
+                [ { [storeName]: listener } ];
+
+              flux.getStore(storeName).listen(listener);
+            });
+            // same as `this.handleStoresChange()` after listening
+            this.listeners.forEach(listenerProps => {
+              const [ storeName ] = Object.keys(listenerProps);
+              const storeListener = listenerProps[storeName];
+              return storeListener();
+            });
+            break;
+        }
+
       }
 
       componentWillUnmount() {
+        const { connectType } = this.state;
         const { flux } = this.context;
-        flux.FinalStore.unlisten(this.handleStoresChange);
+
+        switch (connectType) {
+          case 'reducerFn':
+            flux.FinalStore.unlisten(this.handleStoresChange);
+            break;
+
+          case 'storesState':
+            this.listeners.forEach(listenerProps => {
+              const [ storeName ] = Object.keys(listenerProps);
+              const storeListener = listenerProps[storeName];
+              flux.getStore(storeName).unlisten(storeListener);
+            });
+            this.listeners = null;
+            break;
+        }
       }
 
       takeSnapshot() {
@@ -47,7 +109,24 @@ export default function connectToStores(reducer) {
       }
 
       handleStoresChange = () => {
-        return this.setState({ customProps: reducer(this.takeSnapshot()) });
+        return this.setState({
+          ...this.state,
+          customProps: connectArgs[0](this.takeSnapshot())
+        });
+      }
+
+      handleStoreChange = (storeName) => {
+        const { flux } = this.context;
+
+        return () => {
+          return this.setState({
+            ...this.state,
+            customProps: {
+              ...this.state.customProps,
+              [storeName + 'Store']: flux.getStore(storeName).getState()
+            }
+          });
+        }
       }
 
       render() {
